60f6fca69afe4b713b2ee2830889c6c1f9f7ef36
Sébastien Wilmet <sebastien.wilmet@uclouvain.be>
2016-03-17 15:35:59 +0100

    Meseare latency and receive data

--- pupil_record.m
+++ pupil_record.m
@@ -9,10 +9,15 @@ disp('Start Pupil recording');
 tic;
 zmq_request('send_request', requester, 'start');
 
-reply = zmq_request('receive_reply', requester, 3000)
+reply = zmq_request('receive_reply', requester, 3000);
 if isnan(reply)
 	zmq_request('close');
 	error('Impossible to communicate with the Pupil server.');
+end
+
+if (~strcmp(reply, 'ack'))
+	zmq_request('close');
+	error('Request not acked');
 end
 
 pause(1);
@@ -20,12 +25,28 @@ pause(1);
 disp('Stop Pupil recording');
 zmq_request('send_request', requester, 'stop');
 
-reply = zmq_request('receive_reply', requester, 3000)
+reply = zmq_request('receive_reply', requester, 3000);
 if isnan(reply)
 	zmq_request('close');
 	error('Impossible to communicate with the Pupil server.');
 end
 
-elapsed_time_matlab = toc
+elapsed_time_pupil = str2double(reply);
+elapsed_time_matlab = toc;
+assert(elapsed_time_pupil <= elapsed_time_matlab);
+total_latency_ms = (elapsed_time_matlab - elapsed_time_pupil) * 1000;
+disp(['Latency between Matlab and Pupil: ', num2str(total_latency_ms), ' ms.']);
+if (total_latency_ms > 100)
+	warning('The latency between Matlab and Pupil is too high.');
+end
+
+disp('Receive data...');
+zmq_request('send_request', requester, 'receive_data');
+
+data = zmq_request('receive_reply', requester, 30000)
+if isnan(data)
+	zmq_request('close');
+	error('Timeout, impossible to communicate with the Pupil server.');
+end
 
 zmq_request('close');
